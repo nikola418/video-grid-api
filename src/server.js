dc89f0535b711f70e626e1168e187990018d30b7
Nikola Milovanovic <nikolam998@gmail.com>
2025-02-08 11:06:53 +0100

    versioning

--- src/server.js
+++ src/server.js
@@ -8,16 +8,13 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 server.use(
-  // Add custom route here if needed
-  rewriter({
+  jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
 server.use(router);
-// Listen to port
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
 
-// Export the Server API
 export default server;
